a74123a13bc88aaed6141316dc2169b3a4f73dae
fralonra <fralonra@aliyun.com>
2020-08-17 09:22:37 +0800

    throw if circular deps detected

--- src/tiniocc.ts
+++ src/tiniocc.ts
@@ -1,14 +1,20 @@
 type TInitializeFunc = (container: IoCContainer) => any;
 
 export class IoCContainer {
+  private _deps = new Array<string>();
   private _services = new Map<string, any>();
 
   register(name: string, initializer: TInitializeFunc): IoCContainer {
     Object.defineProperty(this, name, {
       get: () => {
         if (!this._services.has(name)) {
+          if (this._deps.indexOf(name) > -1) {
+            throw new Error('Circular dependency detected.');
+          }
+          this._deps.push(name);
           const module = typeof initializer === 'function' ? initializer(this) : initializer
           this._services.set(name, module);
+          this._deps.pop();
         }
 
         return this._services.get(name);
--- test/test.js
+++ test/test.js
@@ -23,7 +23,7 @@ test('register a module which depends other module', t => {
   t.plan(1)
 
   const tiniocc = createContainer()
-  tiniocc.register('moduleA', "content")
+  tiniocc.register('moduleA', 'content')
   tiniocc.register('moduleB', container => container.moduleA + ' content')
   t.equal(tiniocc.moduleB, 'content content')
 })
@@ -33,6 +33,33 @@ test('register modules correctly regardless of their orders', t => {
 
   const tiniocc = createContainer()
   tiniocc.register('moduleB', container => container.moduleA + ' content')
-  tiniocc.register('moduleA', "content")
+  tiniocc.register('moduleA', 'content')
   t.equal(tiniocc.moduleB, 'content content')
+})
+
+test('throw if there is any circular dependency', t => {
+  t.plan(5)
+
+  const tiniocc = createContainer()
+  tiniocc.register('moduleA', container => container.moduleB)
+  tiniocc.register('moduleB', container => container.moduleA)
+  tiniocc.register('moduleC', container => container.moduleD)
+  tiniocc.register('moduleD', container => container.moduleE)
+  tiniocc.register('moduleE', container => container.moduleC)
+  t.throw(() => tiniocc.moduleA)
+  t.throw(() => tiniocc.moduleB)
+  t.throw(() => tiniocc.moduleC)
+  t.throw(() => tiniocc.moduleD)
+  t.throw(() => tiniocc.moduleE)
+})
+
+test('mix normal modules and circular dependencies', t => {
+  t.plan(2)
+
+  const tiniocc = createContainer()
+  tiniocc.register('module', 'content')
+  tiniocc.register('moduleA', container => container.moduleB)
+  tiniocc.register('moduleB', container => container.moduleA)
+  t.equal(tiniocc.module, 'content')
+  t.throw(() => tiniocc.moduleA)
 })
