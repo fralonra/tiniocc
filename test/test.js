7623c48e368243264c99238090c6f6452f29ef71
fralonra <fralonra@aliyun.com>
2020-08-18 13:46:19 +0800

    add test to cover all branches

--- test/test.js
+++ test/test.js
@@ -19,13 +19,15 @@ test('register a module with a function', t => {
   t.equal(tiniocc.module, 'content')
 })
 
-test('register a module which depends other module', t => {
-  t.plan(1)
+test('register modules which depends on other module', t => {
+  t.plan(2)
 
   const tiniocc = createContainer()
   tiniocc.register('moduleA', 'content')
   tiniocc.register('moduleB', container => container.moduleA + ' content')
+  tiniocc.register('moduleC', container => container.moduleA + ' content')
   t.equal(tiniocc.moduleB, 'content content')
+  t.equal(tiniocc.moduleC, 'content content')
 })
 
 test('register modules correctly regardless of their orders', t => {
